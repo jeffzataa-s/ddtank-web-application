3e493f56c7551e6a5fd04edc779ffda36481b4a6
jeffzataa-sys <jeffzataa2023@hotmail.com>
2023-05-20 19:56:59 -0300

    Update Role

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -6,5 +6,15 @@ use Laratrust\Models\Role as RoleModel;
 
 class Role extends RoleModel
 {
+    const ROLE_SUPERADMIN = "superadmin";
+    const ROLE_ADMIN = "admin";
+    const ROLE_SUPPORT = "support";
+    const ROLE_CUSTOMER = "customer";
+
+    const ROLE_SUPERADMIN_ID = 1;
+    const ROLE_ADMIN_ID = 2;
+    const ROLE_SUPPORT_ID = 3;
+    const ROLE_CUSTOMER_ID = 4;
+
     public $guarded = [];
 }
