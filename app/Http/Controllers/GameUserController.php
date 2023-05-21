9086c62442f22dd8e9ba30b6c92d9a05837a7be2
jeffzataa-sys <jeffzataa2023@hotmail.com>
2023-05-20 21:57:32 -0300

    Implements a GameUserController

---
+++ app/Http/Controllers/GameUserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\GameUserCharacter;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Validator;
+
+class GameUserController extends Controller
+{
+    public function index(Request $request){
+        $validator = Validator::make($request->all(), [
+            'server_id' => "required|exists:servers,id"
+        ]);
+
+        if($validator->fails())
+            return $this->sendError("Validation error", $validator->errors());
+
+        $serverId = $request->input('server_id');
+        $characters = GameUserCharacter::getInfo($serverId, $request->user()->email)->get();
+        return $this->sendResponse($characters, "CharacterCount: {$characters->count()}");
+    }
+}
